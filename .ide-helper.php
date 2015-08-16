4ceb9fe91e4f9663f13898299ab6fa2273f96690
Bernhard Breytenbach <bernhard@coffeecode.co.za>
2015-08-16 23:09:57 +0200

    Update IDE helper

--- .ide-helper.php
+++ .ide-helper.php
@@ -5,6 +5,8 @@ class Mds_ColliveryApi extends \Mds\Prestashop\Collivery\ColliveryApi {}
 class Mds_Install extends \Mds\Prestashop\Installer\Install {}
 class Mds_Uninstall extends \Mds\Prestashop\Installer\Uninstall {}
 class Mds_Service extends \Mds\Prestashop\Settings\Service {}
+class Mds_Surcharge extends Mds\Prestashop\Settings\Surcharge {}
+class Mds_RiskCover extends \Mds\Prestashop\Settings\RiskCover {}
 class Mds_SettingsService extends \Mds\Prestashop\Settings\SettingsService {}
 class Mds_ColliveryException extends \Mds\Prestashop\Exceptions\ColliveryException {}
 class Mds_InvalidData extends \Mds\Prestashop\Exceptions\InvalidData {}
