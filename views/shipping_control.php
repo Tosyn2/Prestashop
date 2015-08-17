76bd4b06552939e85a4107a6d3b7aa74c5456e47
Nicole <nicole@mdstech.co.za>
2015-08-17 14:38:14 +0200

    added view to display mds shipping control

---
+++ views/shipping_control.php
@@ -0,0 +1,12 @@
+<style>
+	.nobootstrap input[type="email"], .nobootstrap input[type="number"] {
+		border: 1px solid #CCC;
+		background-color: #FFF;
+		padding: 2px 4px;
+		box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.1) inset;
+		width: 150px;
+	}
+</style>
+
+<h1>YELLOW</h1>
+
