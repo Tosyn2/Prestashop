1c086140a1f62f3f3758194edb67ef8a546ccbdb
Nicole <nicole@mdstech.co.za>
2015-08-18 11:34:54 +0200

    Creating HTML for mds shipping control page

--- views/shipping_control.php
+++ views/shipping_control.php
@@ -6,7 +6,30 @@
 		box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.1) inset;
 		width: 150px;
 	}
+	
+	.details-block {
+		height:300px;
+		width:300px;
+		border:2px solid black;
+	}
+	
 </style>
 
-<h1>YELLOW</h1>
+<h1>MDS Shipping</h1>
+
+<div class='details-block'>
+<h2>Collection Details</h2>
+Collect from  Address: (  New  |  Saved   )
+</div>
+
+<div class='details-block'>
+<h2>Delivery Details</h2>
+<?= $deliveryAddressId ?>
+</div>
+
+<div class='details-block'>
+<h2>Collivery Details</h2>
+<?= $carrierName ?>
+<?= $serviceId ?>
+</div>
 
