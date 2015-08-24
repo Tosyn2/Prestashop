99e70d22935478ef7ff85319a6c7d2f1c3f3f6d3
Nicole <nicole@mdstech.co.za>
2015-08-24 09:57:42 +0200

    Added section for collection address

--- views/shipping_control.php
+++ views/shipping_control.php
@@ -25,6 +25,71 @@
 								<?php foreach ($deliveryAddresses as $deliveryAddress):  ?>
 								<?php $sql = 'SELECT `name` FROM `' . _DB_PREFIX_ . 'state` WHERE `id_state` = ' . $deliveryAddress['id_state'];
 								$stateName = Db::getInstance()->getValue($sql); ?>
+	<div class="tab-content panel">
+		<div class="tab-pane  in active" id="addressShipping">
+
+
+			<h2>Collection Address</h2>
+
+			<form class="form-horizontal hidden-print" method="post"
+			      action="?controller=AdminOrders&amp;token=<?= $token ?>&amp;vieworder&amp;id_order=<?= $orderId ?>">
+				<div class="form-group">
+					<div class="col-lg-9">
+						<select name="id_address">
+							<?php foreach ($collectionAddresses as $deliveryAddress): ?>
+								<option value="<?= $collectionAddress['id_address'] ?> "
+									<?php if ($collectionAddress['id_address'] == $collectionAddressId) {
+										echo "selected";
+									} ?> >
+									<?= $collectionAddress['alias'] ?> -
+									<?= $collectionAddress['address1'] ?> ,
+									<?= $collectionAddress['city'] ?> ,
+									<?= $collectionAddress['name'] ?>
+								</option>
+							<?php endforeach; ?>
+						</select>
+					</div>
+					<div class="col-lg-3">
+						<button class="btn btn-default" type="submit" name="submitAddressShipping"><i
+								class="icon-refresh"></i>Change
+						</button>
+					</div>
+				</div>
+			</form>
+			<div class="well">
+				<div class="row">
+					<div class="col-sm-6">
+
+						<?php foreach ($CollectionAddresses as $collectionAddress):
+							if ($collectionAddress['id_address'] == $deliveryAddressId) {
+								?>
+								<a class="btn btn-default pull-right"
+								   href="?controller=adminaddresses&amp;id_address=<?= $collectionAddress['id_address'] ?>&amp;updateaddress&amp;token=<?= $token ?>">
+									<i class="icon-pencil"></i>
+									Edit
+								</a>
+								<?php
+								echo
+									$collectionAddress['alias'] . "<br>" .
+									$collectionAddress['address1'] . "<br>" .
+									$collectionAddress['city'] . "<br>" .
+									$collectionAddress['name'];
+							}
+
+							?>
+
+						<?php endforeach; ?>
+
+					</div>
+					<div class="col-sm-6 hidden-print">
+						<div id="map-delivery-canvas" style="height: 190px"></div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
 								<option value="<?= $deliveryAddress['id_address'] ?> "
 									<?php if ($deliveryAddress['id_address'] == $deliveryAddressId) {
 										echo "selected";
