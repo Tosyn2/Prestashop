f1b5403ae4faa2a5225d264daa1bfe8274c154bb
Nicole <nicole@mdstech.co.za>
2015-08-31 17:46:31 +0200

    Fixed styling of view

--- views/shipping_control.php
+++ views/shipping_control.php
@@ -1,54 +1,52 @@
 <div class="row">
-	<ul class="nav nav-tabs" id="tabAddresses">
-		<li class="active">
-			<a href="#addressShipping">
-				<i class="icon-truck"></i>
-				Shipping address
-			</a>
-		</li>
-	</ul>
 	<div class="tab-content panel">
-		<div class="tab-pane  in active" id="addressShipping" style="float:left">
-			<h2>Collection Address</h2>
+		<h1>Shipping Control</h1>
 
-			<form class="form-horizontal hidden-print" method="post"
-			      action="?controller=AdminOrders&amp;token=<?= $token ?>&amp;vieworder&amp;id_order=<?= $orderId ?>&amp;func_name=changeCollectionAddress&amp;value=<?= Tools::getValue(
-				      'id_collection_address'
-			      ) ?>">
-				<div class="form-group">
-					<div class="col-lg-9">
-						<select name="id_collection_address">
-							<?php foreach ($collectionAddresses as $collectionAddress): ?>
-								<option value=<?= $collectionAddress['id_address'] ?>
-									<?php if ($collectionAddress['id_address'] == $collectionAddressId) {
-										echo "selected";
-									} ?>>
-									<?= $collectionAddress['alias'] ?> -
-									<?= $collectionAddress['address1'] ?> ,
-									<?= $collectionAddress['city'] ?> ,
-									<?= $collectionAddress['name'] ?>
-								</option>
-							<?php endforeach; ?>
-						</select>
+		<div class="tab-pane  in active" id="addressShipping" style="display:inline-block; width:100%">
+			<div style="display:inline-block">
+				<h2>Collection Address</h2>
+
+				<form class="form-horizontal hidden-print" method="post"
+				      action="?controller=AdminOrders&amp;token=<?= $token ?>&amp;vieworder&amp;id_order=<?= $orderId ?>&amp;func_name=changeCollectionAddress&amp;value=<?= Tools::getValue(
+					      'id_collection_address'
+				      ) ?>">
+					<div class="form-group">
+						<div class="col-lg-9">
+							<select name="id_collection_address">
+								<?php foreach ($collectionAddresses as $collectionAddress): ?>
+									<option value=<?= $collectionAddress['id_address'] ?>
+										<?php if ($collectionAddress['id_address'] == $collectionAddressId) {
+											echo "selected";
+										} ?>>
+										<?= $collectionAddress['alias'] ?> -
+										<?= $collectionAddress['address1'] ?> ,
+										<?= $collectionAddress['city'] ?> ,
+										<?= $collectionAddress['name'] ?>
+									</option>
+								<?php endforeach; ?>
+							</select>
+						</div>
+						<div class="col-lg-3">
+							<input type="submit" value="Change" class="btn btn-default" class="icon-refresh">
+						</div>
 					</div>
-				</div>
-				<input type="submit" value="Change">
-			</form>
-			<div class="well">
-				<div class="row">
-					<div class="col-sm-6">
-						<?php foreach ($collectionAddresses as $collectionAddress): ?>
-							<?php if ($collectionAddress['id_address'] == $collectionAddressId): ?>
-								<a class="btn btn-default pull-right"
-								   href="?controller=adminaddresses&amp;id_address=<?= $collectionAddress['id_address'] ?>&amp;updateaddress&amp;token=<?= $token ?>">
-									<i class="icon-pencil"></i> Edit
-								</a>
-								<?= $collectionAddress['alias'] ?>
-								<?= $collectionAddress['address1'] ?>
-								<?= $collectionAddress['city'] ?>
-								<?= $collectionAddress['name'] ?>
-							<?php endif; ?>
-						<?php endforeach; ?>
+				</form>
+				<div class="well">
+					<div class="row">
+						<div class="col-sm-6">
+							<?php foreach ($collectionAddresses as $collectionAddress): ?>
+								<?php if ($collectionAddress['id_address'] == $collectionAddressId): ?>
+									<a class="btn btn-default pull-right"
+									   href="?controller=adminaddresses&amp;id_address=<?= $collectionAddress['id_address'] ?>&amp;updateaddress&amp;token=<?= $token ?>">
+										<i class="icon-pencil"></i> Edit
+									</a>
+									<?= $collectionAddress['alias'] ?> <br>
+									<?= $collectionAddress['address1'] ?> <br>
+									<?= $collectionAddress['city'] ?> <br>
+									<?= $collectionAddress['name'] ?> <br>
+								<?php endif; ?>
+							<?php endforeach; ?>
+						</div>
 						<a class="btn btn-default pull-right"
 						   href="?controller=AdminManufacturers&amp;addaddress&amp;token=<?= $token ?>">
 							<i class="icon-pencil"></i> Add Address
@@ -56,7 +54,7 @@
 					</div>
 				</div>
 			</div>
-			<div class="tab-pane  in active" id="addressShipping" style="float:right">
+			<div style="float:right;width:50%">
 				<h2>Delivery Address</h2>
 
 				<form class="form-horizontal hidden-print" method="post"
@@ -81,10 +79,7 @@
 							<button class="btn btn-default" type="submit" name="submitAddressShipping"><i
 									class="icon-refresh"></i>Change
 							</button>
-							<a class="btn btn-default pull-right"
-							   href="?controller=adminaddresses&amp;addaddress&amp;token=<?= $token ?>">
-								<i class="icon-pencil"></i> Add Address
-							</a>
+
 						</div>
 					</div>
 				</form>
@@ -107,23 +102,33 @@
 								}
 								?>
 							<?php endforeach; ?>
+
 						</div>
+
+						<a class="btn btn-default pull-right"
+						   href="?controller=adminaddresses&amp;addaddress&amp;token=<?= $token ?>">
+							<i class="icon-pencil"></i> Add Address
+						</a>
 					</div>
+
 				</div>
-				<form align="center"
-				      action="?controller=AdminOrders&amp;token=<?= $token ?>&amp;vieworder&amp;id_order=<?= $orderId ?>&amp;func_name=getQuote"
-				      method="post">
-					<button>Get Quote</button>
-				</form>
-				<form align="center"
-				      action="?controller=AdminOrders&amp;token=<?= $token ?>&amp;vieworder&amp;id_order=<?= $orderId ?>&amp;func_name=addCollivery"
-				      method="post">
-					<button>Despatch Delivery</button>
-				</form>
-				<?php echo $price; ?>
 			</div>
+			<form align="center"
+			      action="?controller=AdminOrders&amp;token=<?= $token ?>&amp;vieworder&amp;id_order=<?= $orderId ?>&amp;func_name=getQuote"
+			      method="post">
+				<button class="btn btn-default">Get Quote</button>
+			</form>
+			<form align="center"
+			      action="?controller=AdminOrders&amp;token=<?= $token ?>&amp;vieworder&amp;id_order=<?= $orderId ?>&amp;func_name=addCollivery"
+			      method="post">
+				<button class="btn btn-default">Despatch Delivery</button>
+			</form>
+			<?php echo $price; ?>
 		</div>
 	</div>
+	<!--	</div>-->
 </div>
+</div>
+
 
 
