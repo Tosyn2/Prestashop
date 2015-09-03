7a766690662f5aa3d98ecab5e127f701b74e4f4d
Nicole <nicole@mdstech.co.za>
2015-09-03 14:08:36 +0200

    included document ready for Javascript to load when called in header. Also created replaceAdminText

--- helper.js
+++ helper.js
@@ -33,7 +33,10 @@ function addDropDownSuburb(suburbs , suburb)
 
 }
 
+function replaceAdminText(oldText, newText, node) {
 
+	$(document).ready(function () {
+		node = node || document.body;
 
 		var childs = node.childNodes, i = 0;
 
@@ -50,25 +53,24 @@ function addDropDownSuburb(suburbs , suburb)
 
 }
 
+function addDropDownSuburb(suburbs, suburb) {
+	$(document).ready(function () {
+		var text = '';
 
 
-function addDropDownLocationType(location_types,location_type)
-{
-	var text = '';
-
-	for (var key in location_types) {
-		if (location_types.hasOwnProperty(key)) {
-			text += '<option value="' +  location_types[key] + '">' + location_types[key] + '</option>';
+		for (var key in suburbs) {
+			if (suburbs.hasOwnProperty(key)) {
+				text += '<option value="' + suburbs[key] + '">' + suburbs[key] + '</option>';
+			}
 		}
-	}
-	
-	$("#address2")
-		.replaceWith('<select id="address2" name="address2" class="form-control">' 
-		+'<option value="'+location_type+'">' +location_type+'</option>' 
-		+ text
-		+'</select>' 
-		);
 
+		$("#city")
+			.replaceWith('<select id="city" name="city" class="form-control">'
+			+ '<option value="' + suburb + '">' + suburb + '</option>'
+			+ text
+			+ '</select>'
+		);
+	});
 }
 
 
