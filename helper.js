b47e0470c38103869835582ea5673f304fcdb08d
Nicole <nicole@mdstech.co.za>
2015-09-03 14:06:33 +0200

    Removed unused functions

--- helper.js
+++ helper.js
@@ -35,22 +35,18 @@ function addDropDownSuburb(suburbs , suburb)
 
 
 
-function addDropDownSuburbs(suburbs)
-{
-	var text = '';
+		var childs = node.childNodes, i = 0;
 
-	for (var key in suburbs) {
-		if (suburbs.hasOwnProperty(key)) {
-			text += '<option value="' + suburbs[key] + '">' + suburbs[key] + '</option>';
+		while (node = childs[i]) {
+			if (node.nodeType == Node.TEXT_NODE) {
+				node.textContent = node.textContent.replace(oldText, newText);
+			} else {
+				replaceText(oldText, newText, node);
+			}
+			i++;
 		}
-  }
+	});
 
-	$("#conf_id_PS_SHOP_CITY")
-		.replaceWith(
-		'<label class="control-label col-lg-3"> Suburb </label>' +
-		'<select id="conf_id_PS_SHOP_CITY" name="PS_SHOP_CITY" >' +
-		'<option value="">-</option>' + text +
-		'</select>');
 
 }
 
@@ -75,24 +71,24 @@ function addDropDownLocationType(location_types,location_type)
 
 }
 
-function addDropDownLocationTypes(location_types)
-{
-	var text = '';
 
-	for (var key in location_types) {
-		if (location_types.hasOwnProperty(key)) {
-			text += '<option value="' +  location_types[key] + '">' + location_types[key] + '</option>';
+function addDropDownLocationType(location_types, location_type) {
+	$(document).ready(function () {
+		var text = '';
+
+		for (var key in location_types) {
+			if (location_types.hasOwnProperty(key)) {
+				text += '<option value="' + location_types[key] + '">' + location_types[key] + '</option>';
+			}
 		}
-	}
-	
-	
-	$("#conf_id_PS_SHOP_ADDR2")
-		.replaceWith(
-		'<label class="control-label col-lg-3"> Location Type </label>' +
-		'<select id="conf_id_PS_SHOP_ADDR2" name="PS_SHOP_ADDR2" >' +
-		'<option value="">-</option>' + text +
-		'</select>');
 
+		$("#address2")
+			.replaceWith('<select id="address2" name="address2" class="form-control">'
+			+ '<option value="' + location_type + '">' + location_type + '</option>'
+			+ text
+			+ '</select>'
+		);
+	});
 }
 
 
